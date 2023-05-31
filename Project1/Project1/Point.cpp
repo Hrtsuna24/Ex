c14d4937d34d1a0c7ad979cb0226f94707409a10
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-05-31 17:48:18 +0300

    Point constructor

---
+++ Project1/Project1/Point.cpp
@@ -0,0 +1,6 @@
+#include "Point.h"
+
+Point::Point(int x, int y)
+{
+	m_XY[0] = x, m_XY[1] = y;
+}
---
+++ Project1/Project1/Point.h
@@ -0,0 +1,8 @@
+#pragma once
+class Point
+{
+	int m_XY[2]{};
+public:
+	Point(int = 0, int = 0);
+};
+
