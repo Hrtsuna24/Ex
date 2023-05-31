0b59b56dc9c33f0adc14df93fc1e3e82927c0770
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-05-31 19:30:28 +0300

    Point

--- Project1/Project1/Point.cpp
+++ Project1/Project1/Point.cpp
@@ -4,3 +4,14 @@ Point::Point(int x, int y)
 {
 	m_XY[0] = x, m_XY[1] = y;
 }
+
+std::ostream& operator<<(std::ostream& out, const Point & p)
+{
+	// TODO: insert return statement here
+	size_t i{};
+	out << "Point("
+		<< p.m_XY[i++] << ", "
+		<< p.m_XY[i]
+		<< ')';
+	return out;
+}
--- Project1/Project1/Point.h
+++ Project1/Project1/Point.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <ostream>
 class Point
 {
 	int m_XY[2]{};
 public:
 	Point(int = 0, int = 0);
+
+	friend std::ostream& operator<<(std::ostream& out, const Point & p);
 };
 
